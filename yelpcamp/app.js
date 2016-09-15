c3e2a981855ee5bc911d282cdb9e3e76cc4d0c61
henry <henryyogman@gmail.com>
2016-09-15 10:47:05 +0000

    added env var for database

--- yelpcamp/app.js
+++ yelpcamp/app.js
@@ -16,7 +16,8 @@ var commentRoutes = require("./routes/comments");
 var campgroundRoutes = require("./routes/campgrounds");
 var indexRoutes = require("./routes/index");
     
-mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect(process.env.DATABASEURL);
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
